5f09f7ddc166adc10f8aa16a7ebc4cf77c8b4ce7
Mik259 <111299128+Mik259@users.noreply.github.com>
2022-08-27 12:01:20 +0200

    Updated code
    Updated code

--- main.c
+++ main.c
@@ -21,12 +21,10 @@ int main(){
     // prompt to enter the intger number 
     int runner=1;
     while(runner==1){
-    printf("Enter a decimal number: ");
+    printf("\nEnter a decimal number: ");
     scanf("%d", &n);
-    if (n>0){
+    if (n>=0){
 
-        //int conv=0;
-        //conv=1;
         printf("The number you have entered is %d\n",n);
 
 
@@ -40,7 +38,7 @@ int main(){
         float l2;
         int rem,div;
         l2 = log2(n);
-        printf("The log2 of the number is %f\n",l2);
+        printf("The log2 of the number is %.2f\n",l2);
         div=n/radix;
         printf("The integer result if the number divided by %d is %d\n",radix,div);
         rem= n%radix;
@@ -54,40 +52,25 @@ int main(){
 
 void Dec2RadI(int n,int radix){
 
-
+        // code given in practical session to create an array to store converted value
         int conv=1;
         int bits=sizeof(conv)*8;
         char bin[sizeof(conv)*8+1]; // bin is a pointer iself
-        bin[sizeof(conv)*8]= '\0'; // stack allocation of memory, array is destroyed when we return from scope form which it was created. 
-
-        for(int i=0;i<bits;i++){
+        bin[sizeof(conv)*8]= '\0'; // stack allocation of memory, array is destroyed when we return from scope form which it was created.
+        
+        printf("The radix-%d value is ",radix);
+        int i=0; 
+        if (n==0){printf("0");}
+        while (n!=0){
 
             int r;
             char basechars[] = "0123456789ABCDEF";
+            // conversion using continuous division method. 
             r=n%radix;
             n=n/radix;
-            bin[i] = basechars[r];}
-
-        //reversal of bin array was not sure why the conversion was coming out in reverse 
-        int temp;
-        for(int i = 0; i<bits/2; i++){    
-            temp = bin[i];
-            bin[i] = bin[bits-i-1];
-            bin[bits-i-1] = temp;}
-        printf("%s\n",bin);}
-
-
-
-
-
-
-
-
-
-
-
-
-
+            bin[i++] = basechars[r];}
+            for (int k = i - 1; k >= 0; --k) {
+            printf("%c", bin[k]);}}
 
 // Some code given in the practical session but not used 
 //const int index= bits-i-1; // forces compiler to complie once and only once
